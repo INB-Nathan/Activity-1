bbc69760c4af2a6e924d3e5113aacdc303ec9fb4
INB-Nathan <202312375@fit.edu.ph>
2024-08-01 17:48:53 +0800

    feat(navbar): add navbar in every page

---
+++ app/services/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../services/components/NavBar'
+
+const page = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Services page</h1>
+    </div>
+  )
+}
+
+export default page
